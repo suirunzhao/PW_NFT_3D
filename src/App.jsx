a6320ca293a5241d6e2d9822f7ac7acd6bcb67f4
chiusuiyuen <90884217+chiusuiyuen@users.noreply.github.com>
2023-04-29 20:19:54 +0800

    fixed text

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { useCursor, MeshReflectorMaterial, Image, Text, Environment } from '@rea
 import { useRoute, useLocation } from 'wouter'
 import { easing } from 'maath'
 import getUuid from 'uuid-by-string'
-import pexel from './main';
 
 const GOLDENRATIO = 1
 
@@ -97,8 +96,6 @@ function Frame({ url, c = new THREE.Color(), ...props }) {
         <Image raycast={() => null} ref={image} position={[0, 0, 0.7]} url={url} />
       </mesh>
       <Text maxWidth={0.1} anchorX="left" anchorY="top" position={[0.55, GOLDENRATIO, 0]} fontSize={0.025}>
-        NFT 
-        by Siri
       </Text>
     </group>
   )
