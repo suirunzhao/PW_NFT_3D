a5fa043a4d13cd37c0644c0bc1d3b7775867b9be
chiusuiyuen <90884217+chiusuiyuen@users.noreply.github.com>
2023-04-29 20:30:23 +0800

    phone version

--- src/main.jsx
+++ src/main.jsx
@@ -11,18 +11,18 @@ const matchResult = window.matchMedia("(max-width: 1400px)");
 if (matchResult.matches) {
   const images = [
     // Front
-    { position: [0, 1.2, -2], rotation: [0, 0, 0], url: pexel(0) },
+    { position: [0, 1.2, -4], rotation: [0, 0, 0], url: pexel(0) },
     // Back
-    { position: [-1.25, 1.2, -2], rotation: [0, 0, 0], url: pexel(7) },
-    { position: [1.25, 1.2, -2], rotation: [0, 0, 0], url: pexel(5) },
+    { position: [-1.25, 1.2, -4], rotation: [0, 0, 0], url: pexel(7) },
+    { position: [1.25, 1.2, -4], rotation: [0, 0, 0], url: pexel(5) },
     // Left
-    { position: [-1.25, 2.4, -2], rotation: [0, 0, 0], url: pexel(3) },
-    { position: [0, 2.4, -2], rotation: [0, 0, 0], url: pexel(4) },
-    { position: [1.25, 2.4, -2], rotation: [0, 0, 0], url: pexel(1) },
+    { position: [-1.25, 2.4, -4], rotation: [0, 0, 0], url: pexel(3) },
+    { position: [0, 2.4, -4], rotation: [0, 0, 0], url: pexel(4) },
+    { position: [1.25, 2.4, -4], rotation: [0, 0, 0], url: pexel(1) },
     // Right
-    { position: [-1.25, 0, -2], rotation: [0, 0, 0], url: pexel(6) },
-    { position: [0, 0, -2], rotation: [0, 0, 0], url: pexel(2) },
-    { position: [1.25, 0, -2], rotation: [0, 0, 0], url: pexel(9) }
+    { position: [-1.25, 0, -4], rotation: [0, 0, 0], url: pexel(6) },
+    { position: [0, 0, -4], rotation: [0, 0, 0], url: pexel(2) },
+    { position: [1.25, 0, -4], rotation: [0, 0, 0], url: pexel(9) }
   ]
   createRoot(document.getElementById('root')).render(<App images={images} />)
 } else {
